08d622ed8b19e5a858c086810a2bd97231fb26ec
Dominik Dumont <dominik.dumont@aoe.com>
2018-08-24 21:15:18 +0200

    Update package main entry

--- packages/flamingo-carotene-pug/lib/flamingo-carotene-pug.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = flamingoCarotenePug;
-
-function flamingoCarotenePug() {
-    // TODO
-}
--- packages/flamingo-carotene-webpack/lib/flamingo-carotene-webpack.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = flamingoCaroteneWebpack;
-
-function flamingoCaroteneWebpack() {
-    // TODO
-}
