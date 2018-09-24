ea7dbb068cb75ddec39d73b1d43ae21b0f2d407a
Benjamin Egner <benjamin.egner@aoe.com>
2018-09-24 10:47:51 +0200

    Fixed Modules with building "dist"

---
+++ packages/flamingo-carotene-behavior/dist/behavior.js
@@ -0,0 +1,278 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+/**
+ *
+ */
+var Behavior =
+/*#__PURE__*/
+function () {
+  /**
+   * @param listOfBehaviourClasses
+   *
+   * Webpack Globbing: import behaviorModules from '*.behavior.js'
+   * behaviourClasses is an array, which holds esModules and a "default" class of behaviors in it
+   * 0: default: named function
+   * 1: default: named function
+   * ...
+   *
+   * OR
+   *
+   * Babel Globbing: import * as behaviorModules from '*.behavior.js'
+   * Object of Classes - result of babel globbing
+   * {
+   *   anyName: named function
+   *   anotherName: named function
+   *   ...
+   * }
+   */
+  function Behavior(behaviourClasses) {
+    _classCallCheck(this, Behavior);
+
+    /**
+     * List of all registered behaviors
+     * @type {{}}
+     */
+    this.behaviors = {};
+    /**
+     * Internal Flag, which outputs console logs of behavior functionality
+     * @type {boolean}
+     */
+
+    this.debug = false;
+
+    for (var behaviorClassObjectIndex in behaviourClasses) {
+      var behaviorClassObject = behaviourClasses[behaviorClassObjectIndex];
+      var behaviorClass = null; // Support for globbing: import behaviorModules from '...'
+
+      if (behaviorClassObject.hasOwnProperty('__esModule') && behaviorClassObject.__esModule === true) {
+        if (behaviorClassObject.hasOwnProperty('default')) {
+          behaviorClass = behaviorClassObject.default;
+        }
+      } else {
+        // Support for globbing: import * as behaviorModules from '...'
+        behaviorClass = behaviorClassObject;
+      } // if class has got a name
+
+
+      if (behaviorClass.hasOwnProperty('name')) {
+        var className = this._sanitizeName(behaviorClass.name);
+
+        this.behaviors[className] = behaviorClass;
+      }
+    }
+
+    if (this.debug) {
+      console.info("Registered behaviors:", this.behaviors);
+    }
+  }
+  /**
+   * Attachs a Behavior to given domElement (and all children)
+   * @param domElement|null
+   */
+
+
+  _createClass(Behavior, [{
+    key: "attachBehaviors",
+    value: function attachBehaviors(domElement) {
+      domElement = domElement || document;
+      var allElements = domElement.querySelectorAll('[data-behavior]');
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = allElements[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var behaviorElement = _step.value;
+
+          this._attachBehaviorToDom(behaviorElement);
+        }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return != null) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
+      }
+    }
+    /**
+     * Detach all Behaviors to given domElement (and all children)
+     * @param domElement
+     */
+
+  }, {
+    key: "detachBehaviors",
+    value: function detachBehaviors(domElement) {
+      domElement = domElement || document;
+      var allElements = domElement.querySelectorAll('[data-behavior]');
+      var _iteratorNormalCompletion2 = true;
+      var _didIteratorError2 = false;
+      var _iteratorError2 = undefined;
+
+      try {
+        for (var _iterator2 = allElements[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+          var behaviorElement = _step2.value;
+
+          this._detachBehaviorsToDom(behaviorElement);
+        }
+      } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion2 && _iterator2.return != null) {
+            _iterator2.return();
+          }
+        } finally {
+          if (_didIteratorError2) {
+            throw _iteratorError2;
+          }
+        }
+      }
+    }
+    /**
+     * Returns a behavior instance from a dom Element
+     * @param domElement
+     * @returns {*}
+     */
+
+  }, {
+    key: "getBehaviorFromElement",
+    value: function getBehaviorFromElement(domElement) {
+      var instance = null;
+
+      if (domElement.hasOwnProperty('nodeType')) {
+        if (this.debug) {
+          console.error("Behavior: attachBehaviorToElement: No valid domElement given", domElement);
+        }
+      }
+
+      if (domElement.hasOwnProperty('behaviorInstance')) {
+        instance = domElement.behaviorInstance;
+      }
+
+      return instance;
+    }
+    /**
+     * Attachs a behavior to a given dom element
+     * @param domElement
+     */
+
+  }, {
+    key: "_attachBehaviorToDom",
+    value: function _attachBehaviorToDom(domElement) {
+      var instance = this.getBehaviorFromElement(domElement);
+
+      if (instance) {
+        if (this.debug) {
+          console.error('Behavior: attachBehaviorToElement: Element already initialized', instance);
+        }
+
+        return;
+      }
+
+      var behaviorName = this._sanitizeName(domElement.dataset.behavior);
+
+      if (!this.behaviors.hasOwnProperty(behaviorName)) {
+        if (this.debug) {
+          console.error("Behavior: attachBehaviorToElement: Behavior \"".concat(behaviorName, "\" should be attached, but not registered. Check your spelling."));
+        }
+
+        return;
+      }
+
+      var BehaviorClass = this.behaviors[behaviorName];
+
+      if (this.debug) {
+        console.info("Behavior: attachBehaviorToElement: Initialize behavior \"".concat(behaviorName, "\" on domElement"), domElement);
+      }
+
+      domElement.behaviorInstance = {
+        name: behaviorName,
+        behavior: new BehaviorClass(domElement)
+      };
+    }
+    /**
+     * Detach a behavior from a given dom element
+     * @param domElement
+     */
+
+  }, {
+    key: "_detachBehaviorsToDom",
+    value: function _detachBehaviorsToDom(domElement) {
+      var instance = this.getBehaviorFromElement(domElement);
+
+      if (!instance) {
+        if (this.debug) {
+          console.error("Behavior: detachBehaviors: Cant detach element, there is no behavior present.", instance);
+        }
+
+        return;
+      }
+
+      var behaviorName = this._sanitizeName(instance.name);
+
+      if (this.debug) {
+        console.info("Behavior: detachBehaviors: Destroy behavior \"".concat(behaviorName, "\" on domElement"), domElement);
+      }
+
+      var destroyed = false; // check if there is a destroy method
+
+      if (typeof instance.behavior.destroy === 'function') {
+        instance.behavior.destroy();
+        destroyed = true;
+      } // check if there is a dispose method
+
+
+      if (typeof instance.behavior.dispose === 'function') {
+        instance.behavior.dispose();
+        destroyed = true;
+      }
+
+      if (!destroyed) {
+        if (this.debug) {
+          console.error("Behavior: detachBehaviors: Cannot call destroy/dispose on \"".concat(behaviorName, "\" - methods not found."), domElement);
+        }
+      }
+
+      domElement.behaviorInstance = null;
+    }
+    /**
+     * Sanatizing Name for Behavior ClassNames and Data Attributes
+     * @param {string} behaviorName
+     * @returns {string}
+     */
+
+  }, {
+    key: "_sanitizeName",
+    value: function _sanitizeName(behaviorName) {
+      return behaviorName.toLowerCase();
+    }
+    /**
+     * Enables Debug Mode
+     * @param state {boolean}
+     */
+
+  }, {
+    key: "setDebug",
+    value: function setDebug(state) {
+      this.debug = state;
+    }
+  }]);
+
+  return Behavior;
+}();
+
+module.exports = Behavior;
---
+++ packages/flamingo-carotene-state-manager/dist/stateManager.js
@@ -0,0 +1,144 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _redux = require("redux");
+
+var _objectPath = require("object-path");
+
+var _reduxWatch = _interopRequireDefault(require("redux-watch"));
+
+var _deepEqual = _interopRequireDefault(require("deep-equal"));
+
+var _objectPathImmutable = _interopRequireDefault(require("object-path-immutable"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+/**
+ * Generic reducer with the ability to set values in the state tree
+ * @param state {Object} The current state object
+ * @param action.type {String} Generic action type to set value. 'applicationStore.SET' ist the only available action type.
+ * @param action.path {String} Path to the part of the state object that should be changed
+ * @param action.value {Object} Value of the change
+ * @return {Object} The new state object
+ */
+var rootReducer = function rootReducer() {
+  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  var action = arguments.length > 1 ? arguments[1] : undefined;
+
+  if (action.type.startsWith('SET:') && action.path !== undefined) {
+    return _objectPathImmutable.default.set(state, action.path, action.value);
+  }
+
+  return state;
+};
+/**
+ * Internal wrapper for deepEqual with strict option set
+ * @param {Object} value1 First comparand
+ * @param {Object} value2 Second comparand
+ * @return {Boolean} Result of equality comparison
+ */
+
+
+function deepEqualStrict(value1, value2) {
+  return (0, _deepEqual.default)(value1, value2, {
+    strict: true
+  });
+}
+/**
+ * Application state manager. Can store application wide state information and publish changes.
+ */
+
+
+var State =
+/*#__PURE__*/
+function () {
+  function State() {
+    _classCallCheck(this, State);
+
+    this.store = null;
+  }
+  /**
+   * Initialize application state with js-object. To be called only once before usage of state instance.
+   * @param {Object} initialState Object containing the initial application state
+   * @return {void}
+   */
+
+
+  _createClass(State, [{
+    key: "init",
+    value: function init(initialState) {
+      if (!this.store) {
+        this.store = (0, _redux.createStore)(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+      } else {
+        throw new Error('State: Store is already initialized');
+      }
+    }
+    /**
+     * Subscribe to changes in specific parts of the store.
+     * @param {String} path String in object notation to store part of interest
+     * @param {Function} callback Callback function for store changes
+     * @return {Function} Function which can be used to unsubscribe callback
+     */
+
+  }, {
+    key: "watch",
+    value: function watch(path, callback) {
+      // check if path param is used as callback without specific path (watch whole store)
+      var callbackFunc = typeof path === 'function' ? path : callback;
+      var pathStr = typeof path === 'string' ? path : ''; // create watcher and subscribe to store
+
+      var watchFunc = (0, _reduxWatch.default)(this.store.getState, pathStr, deepEqualStrict);
+      return this.store.subscribe(watchFunc(callbackFunc));
+    }
+    /**
+     * Get state value by opject path string
+     * @param {String} path String in object notation to store value or subtree
+     * @return {Object} Value of given object path or undefined
+     */
+
+  }, {
+    key: "get",
+    value: function get() {
+      var path = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+      return (0, _objectPath.get)(this.store.getState(), path);
+    }
+    /**
+     * Set state value of object property defined by object path string
+     * @param {String} path String in object notation to store value
+     * @param {Object} value Value that should be set to specified path
+     * @return {void}
+     */
+
+  }, {
+    key: "set",
+    value: function set(path, value) {
+      var currentValue = this.get(path);
+      var newValueEqualsCurrentValue = deepEqualStrict(currentValue, value); // Only dispatch action if change occurs
+
+      if (!newValueEqualsCurrentValue) {
+        this.store.dispatch({
+          type: 'SET: ' + path,
+          path: path,
+          value: value
+        });
+      }
+    }
+  }]);
+
+  return State;
+}(); // Export singleton instance
+
+
+var _default = new State();
+
+exports.default = _default;
