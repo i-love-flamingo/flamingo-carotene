980bd6cc3a714accf7737c83fb848f0833da1ebc
Dominik Dumont <dominik.dumont@aoe.com>
2018-10-23 11:09:47 +0200

    Move ESLint file detection to config command

--- packages/flamingo-carotene-es-lint/caroteneModule.js
+++ packages/flamingo-carotene-es-lint/caroteneModule.js
@@ -1,4 +1,13 @@
-const buildHandler = require('./lib/handler/build')
+const path = require('path')
+const fs = require('fs')
+
+const lintHandler = require('./lib/handler/build')
+
+const configFileNames = ['.eslintrc', '.eslintrc.js', '.eslintrc.yaml', '.eslintrc.yml', '.eslintrc.json']
+const ignoreFileNames = ['.eslintignore']
+
+const defaultConfigFileName = '.eslintrc.js'
+const defaultIgnoreFileName = '.eslintignore'
 
 // class CarotenePug extends CaroteneModule {
 class ESLint {
@@ -11,32 +20,30 @@ class ESLint {
         handler: function (core) {
           const config = core.getConfig()
 
-          if (!config.eslint) {
-            config.eslint = {}
-          }
-          config.eslint.breakOnError = false
-          config.paths.esLint = __dirname
+          config.paths.eslint = __dirname
+
+          config.eslint = {}
+          config.eslint['useWebpackLoader'] = true
+          config.eslint['breakOnError'] = false
+          config.eslint['configFilePath'] = getESLintFilePath(configFileNames, defaultConfigFileName, config)
+          config.eslint['ignoreFilePath'] = getESLintFilePath(ignoreFileNames, defaultIgnoreFileName, config)
         }
       },
       {
         command: 'lint',
-        handler: buildHandler
-      },
-      {
-        command: 'buildTemplates',
-        handler: buildHandler
+        handler: lintHandler
       },
       {
         command: 'build',
         handler: function (core) {
           const config = core.getConfig()
           config.eslint.breakOnError = true
-          buildHandler(core)
+          lintHandler(core)
         }
       },
       {
         command: 'watchWebpackJs',
-        handler: buildHandler
+        handler: lintHandler
       }
     ]
   }
@@ -44,6 +51,23 @@ class ESLint {
   getListeners () {
     return this.listeners
   }
+}
+
+/**
+ * Loads a config file from the project and if there does not exist such a config file, a default file will be loaded.
+ * @param fileNames Array of file names to check
+ * @param defaultFileName Name of the default file located in this package
+ * @param config The flamingo carotene config
+ */
+getESLintFilePath = (fileNames, defaultFileName, config) => {
+  for (const fileName of fileNames) {
+    const filePath = path.join(config.paths.project, fileName)
+    if (fs.existsSync(filePath)) {
+      return filePath
+    }
+  }
+
+  return path.join(config.paths.esLint, defaultFileName)
 }
 
 module.exports = ESLint
--- packages/flamingo-carotene-es-lint/lib/handler/build.js
+++ packages/flamingo-carotene-es-lint/lib/handler/build.js
@@ -1,21 +1,15 @@
 const { spawn } = require('child_process')
-const path = require('path')
-const fs = require('fs')
 
 /**
  * Executes the es lint
  * @param core
  */
-const esLint = (core) => {
+const eslint = (core) => {
   const cliTools = core.getCliTools()
   const config = core.getConfig()
 
-  // trying to find es lint in project folder..
-  let rulesConfigFilePath = loadConfigPath('.eslintrc.js', config)
-  let ignoreConfigFilePath = loadConfigPath('.eslintignore', config)
-
   const cmd = `yarn`
-  const parameters = ['eslint', '--config', rulesConfigFilePath, '--ignore-path', ignoreConfigFilePath, '--ext', '.js', '.']
+  const parameters = ['eslint', '--config', config.eslint.configFilePath, '--ignore-path', config.eslint.ignoreFilePath, '--ext', '.js', '.']
 
   cliTools.info('ESLint - start')
 
@@ -72,21 +66,4 @@ const esLint = (core) => {
   })
 }
 
-/**
- * Loads a config file from the project and if there does not exist such a config file, a default file will be loaded.
- * @param configName Complete name of the config file which should be loaded.
- * @param config The config used get the paths.
- */
-const loadConfigPath = (configName, config) => {
-  // trying to find es lint in project folder..
-  let configFilePath = path.join(config.paths.project, configName)
-
-  // if not exists, take standard one
-  if (!fs.existsSync(configFilePath)) {
-    configFilePath = path.join(config.paths.esLint, configName)
-  }
-
-  return configFilePath
-}
-
-module.exports = esLint
+module.exports = eslint
