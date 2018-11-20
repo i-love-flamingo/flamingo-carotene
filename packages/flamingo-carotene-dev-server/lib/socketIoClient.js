3099101ddb8bb317fea7e9116aa8b79879d0b060
Stanislav Müller <stanislav.mueller@aoe.com>
2018-11-20 10:44:47 +0100

    Dev server sockets now work with any hostname

--- packages/flamingo-carotene-dev-server/lib/socketIoClient.js
+++ packages/flamingo-carotene-dev-server/lib/socketIoClient.js
@@ -1,5 +1,6 @@
 var io = require('socket.io-client')
-var socket = io('http://localhost:3000')
+var sockeUri = window.location.protocol + '//' + window.location.hostname + ':3000'
+var socket = io(sockeUri)
 
 socket.on('connect', function onConnect () {
   console.info('Connected socket with id: ' + socket.id)
