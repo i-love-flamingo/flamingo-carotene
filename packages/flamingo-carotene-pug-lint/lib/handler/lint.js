b5fd7e6441e76e569859d77f06095f3a235c60b3
Benjamin Egner <benjamin.egner@aoe.com>
2019-01-15 09:04:29 +0100

    [TASK] #185031 Development on Windows Prevent "Cmd line too long" on windows cmd

--- packages/flamingo-carotene-pug-lint/lib/handler/lint.js
+++ packages/flamingo-carotene-pug-lint/lib/handler/lint.js
@@ -1,71 +1,111 @@
-const { spawn } = require('child_process')
+const {spawn} = require('child_process')
 const path = require('path')
 const fs = require('fs')
 const glob = require('glob')
 
+// limit cmd length (note: windows cmd has a limit of 8000 chars)
+const maxShellCmdLimit = 7000;
+
 const pugLint = (core) => {
-    const cliTools = core.getCliTools()
-    const config = core.getConfig()
+  const cliTools = core.getCliTools()
+  const config = core.getConfig()
 
-    // trying to find sass lint in project folder..
-    let configFile = path.join(config.paths.project, '.pug-lintrc.js')
+  // trying to find sass lint in project folder..
+  let configFile = path.join(config.paths.project, '.pug-lintrc.js')
 
-    // if not exists, take standard one
-    if (!fs.existsSync(configFile)) {
-        configFile = path.join(config.paths.pugLint, '.pug-lintrc.js')
+  // if not exists, take standard one
+  if (!fs.existsSync(configFile)) {
+    configFile = path.join(config.paths.pugLint, '.pug-lintrc.js')
+  }
+  if (process.platform === 'win32') {
+    configFile = configFile.split('\\').join('/')
+  }
+
+  // get all files to be linted...
+  const files = []
+  glob.sync(`${config.paths.src}/${config.pugLint.filesPattern}`).forEach((file) => {
+    if (process.platform === 'win32') {
+      files.push(file.split('\\').join('/'))
     }
+    else {
+      files.push(path.resolve(file))
+    }
+  })
 
-    const files = []
-    glob.sync(`${config.paths.src}/${config.pugLint.filesPattern}`).forEach((file) => {
-        files.push(path.resolve(file))
-    })
+  // get cmd
+  let cmd = 'pug-lint'
+  if (process.platform === 'win32') {
+    cmd = `pug-lint.cmd`
+  }
 
-    const cmd = 'pug-lint'
-    const parameters = ['--config', configFile].concat(files)
-    cliTools.info('PugLint - start')
+  cliTools.info(`PugLint - start (${files.length} files`)
 
-    const spawnEnv = process.env
-    spawnEnv.FORCE_COLOR = true
+  const spawnEnv = process.env
+  spawnEnv.FORCE_COLOR = true
 
-    const childProcess = spawn(
-        cmd,
-        parameters,
-        {
-            env: spawnEnv
-        }
-    )
+  // create shell cmd "packages" (respect maxShellCmdLimit)
+  const lintFilePacks = []
+  let packNumber = 0
+  for (let file of files) {
+    if (!lintFilePacks[packNumber]) {
+      lintFilePacks[packNumber] = []
+    }
+    if (lintFilePacks[packNumber].join('').length > maxShellCmdLimit) {
+      packNumber++
+      lintFilePacks[packNumber] = []
+    }
+    lintFilePacks[packNumber].push(file)
+  }
 
-    const results = []
-    const errors = []
+
+
+  const results = []
+  const errors = []
+  let finishedPacks = 0;
+
+  // spawn puglint for each pack
+  for (let lintFilePackIndex in lintFilePacks) {
+    const lintFilePack = lintFilePacks[lintFilePackIndex];
+    const lintPackNumber = parseInt(lintFilePackIndex, 10) + 1;
+    cliTools.info(`Start PugLint-Pack ${lintPackNumber} with ${lintFilePack.length} files...`);
+
+    const childProcess = spawn(cmd, ['--config', configFile].concat(lintFilePack), {env: spawnEnv})
 
     childProcess.stdout.on('data', function (data) {
-        results.push(data)
+      results.push(data)
     })
 
     childProcess.stderr.on('data', function (data) {
-        const errorPrefix = `${config.paths.project}/`
-        const lines = data.toString().split("\n").map((line) => {
-            if (line.includes(errorPrefix)) {
-                return `⚠️   ${line.split(errorPrefix).join('')}`
-            }
-            return line
-        })
-        errors.push(lines.join("\n"))
+      const errorPrefix = `${config.paths.project}/`
+      const lines = data.toString().split("\n").map((line) => {
+        if (line.includes(errorPrefix)) {
+          return `⚠️   ${line.split(errorPrefix).join('')}`
+        }
+        return line
+      })
+      errors.push(lines.join("\n"))
     })
 
     childProcess.on('exit', (code) => {
-        const output = ['PugLint - end'].concat(results, errors).join('\n').trim()
+      finishedPacks++;
+      cliTools.info(`Finished PugLint-Pack ${lintPackNumber} - (${finishedPacks} of ${lintFilePacks.length})`);
+
+      // if every pack is finished
+      if (finishedPacks >= lintFilePacks.length) {
+        const output = [`PugLint-Lint End`].concat(results, errors).join('\n').trim()
 
         if (code !== 0) {
-            cliTools.warn(output)
+          cliTools.warn(output)
         } else {
-            cliTools.info(output)
+          cliTools.info(output)
         }
 
         if (config.pugLint.breakOnError && errors.length > 0) {
-            core.reportError(`PugLint report errors.`)
+          core.reportError(`PugLint report errors.`)
         }
+      }
     })
+  }
 }
 
 module.exports = pugLint
