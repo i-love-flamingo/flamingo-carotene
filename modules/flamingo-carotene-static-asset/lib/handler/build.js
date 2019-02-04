4af3fe10318d3e451055b40878e50913a0eeb4c5
Dylan Provost <dylan.provost@aoe.com>
2019-02-04 11:29:34 +0100

    TASK #191934 Refactor copy asset module

--- modules/flamingo-carotene-static-asset/lib/handler/build.js
+++ modules/flamingo-carotene-static-asset/lib/handler/build.js
@@ -3,17 +3,21 @@ const async = require('async')
 const mkdirp = require('mkdirp')
 const path = require('path')
 const shell = require('shelljs')
+const os = require('os')
 
 let config
 let cliTools
 
+/**
+ * Creates an anonymous function for the async mapLimit that takes a destination path as an additional parameter. It then proceeds to copy the file to the destination path
+ * @param destPath
+ * @returns {Function}
+ */
 const generateCopyAsset = (destPath) => {
-  cliTools.info(path.dirname(destPath))
-  return (srcFileName, callback) => {
+  return (srcFilePath, callback) => {
     try {
       mkdirp(path.dirname(destPath), () => {
-        cliTools.log(`        asset > ${srcFileName} to ${destPath}`, true)
-        // shell.cp(srcFileName, destPath)
+        shell.cp('-r', srcFilePath, destPath)
       })
     } catch (e) {
       cliTools.warn(e)
@@ -22,23 +26,35 @@ const generateCopyAsset = (destPath) => {
   }
 }
 
-const globFiles = (filePath, destPath) => {
-  glob(filePath, (error, files) => {
-    if (error) {
-      cliTools.warn(error)
-      return
-    }
-
-    const threadCount = require('os').cpus().length || 2
-    async.mapLimit(files, threadCount, generateCopyAsset(destPath), (error, results) => {
+/**
+ * Globs all files of srcPath and iterates through each of them asynchronously to copy them to destination path
+ * @param srcPath
+ * @param destPath
+ * @returns {Promise<any>}
+ */
+const globFiles = (srcPath, destPath) => {
+  return new Promise((resolve, reject) => {
+    glob(srcPath, (error, files) => {
       if (error) {
-        cliTools.warn(error)
+        reject(error)
         return
       }
+
+      const threadCount = os.cpus().length || 2
+      async.mapLimit(files, threadCount, generateCopyAsset(destPath), (error, result) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(result)
+      })
     })
   })
 }
 
+/**
+ * Main function that runs on build
+ * @param core
+ */
 const build = (core) => {
   config = core.getConfig()
   cliTools = core.getCliTools()
@@ -47,7 +63,7 @@ const build = (core) => {
     const assetSrcBasePath = config.staticAsset.paths.src
     const assetTargetBasePath = config.staticAsset.paths.dist
 
-    // Copy assets
+    // Creates base directory
     mkdirp(assetTargetBasePath, function (err) {
       if (err) {
         cliTools.warn(error)
@@ -56,11 +72,17 @@ const build = (core) => {
       cliTools.info('Copy static assets - start')
       const timeStarted = new Date().getTime()
 
+      // Loops through each item to glob
       config.staticAsset.link.forEach((currentLinks) => {
-        globFiles(path.join(assetSrcBasePath, currentLinks.src), path.join(assetTargetBasePath, currentLinks.dist))
+        // Globs files from current iterations of paths
+        globFiles(path.join(assetSrcBasePath, currentLinks.src), path.join(assetTargetBasePath, currentLinks.dist)).then((result) => {
+          if (result) {
+            cliTools.info(`Copy static assets - end\r\n   Finished after ${new Date().getTime() - timeStarted}ms`)
+          } else {
+            cliTools.warn(result)
+          }
+        })
       })
-
-      cliTools.info(`Copy static assets - end\r\n   Finished after ${new Date().getTime() - timeStarted}ms`)
     })
   }
 }
