838558448994d1a8c30143f754b8f4c67e3e4efc
Benjamin Egner <5068180+begner@users.noreply.github.com>
2019-10-21 10:18:47 +0200

    use dev-cache only on dev environment

--- modules/flamingo-carotene-webpack/lib/webpackConfig.js
+++ modules/flamingo-carotene-webpack/lib/webpackConfig.js
@@ -13,24 +13,28 @@ class WebpackConfig {
   }
 
   getCacheConfig () {
-    return {
-      info: {
-        // 'none' or 'test'.
-        mode: 'none',
-        // 'debug', 'log', 'info', 'warn', or 'error'.
-        level: (this.cliTools.isVerbose() ? 'debug' : 'error'),
-      },
-      // Clean up large, old caches automatically.
-      cachePrune: {
-        // Caches younger than `maxAge` are not considered for deletion. They must
-        // be at least this (default: 2 days) old in milliseconds.
-        maxAge: 2 * 24 * 60 * 60 * 1000,
-        // All caches together must be larger than `sizeThreshold` before any
-        // caches will be deleted. Together they must be at least this
-        // (default: 50 MB) big in bytes.
-        sizeThreshold: 50 * 1024 * 1024
+    if (process.env.NODE_ENV === 'development') {
+      return {
+        info: {
+          // 'none' or 'test'.
+          mode: 'none',
+          // 'debug', 'log', 'info', 'warn', or 'error'.
+          level: (this.cliTools.isVerbose() ? 'debug' : 'error'),
+        },
+        // Clean up large, old caches automatically.
+        cachePrune: {
+          // Caches younger than `maxAge` are not considered for deletion. They must
+          // be at least this (default: 2 days) old in milliseconds.
+          maxAge: 2 * 24 * 60 * 60 * 1000,
+          // All caches together must be larger than `sizeThreshold` before any
+          // caches will be deleted. Together they must be at least this
+          // (default: 50 MB) big in bytes.
+          sizeThreshold: 50 * 1024 * 1024
+        }
       }
     }
+
+    return null
   }
 
   getWebpackConfig () {
