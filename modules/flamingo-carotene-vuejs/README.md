09bb6de013d0a05faa29faeee5d3fc044fef1691
Dylan Provost <dylan.provost@aoe.com>
2020-05-05 08:54:10 +0200

    Start setting up VueJs module

---
+++ modules/flamingo-carotene-vuejs/README.md
@@ -0,0 +1,17 @@
+# Module `flamingo-carotene-vuejs`
+
+This module provides a wrapper to add VueJS dependencies to a Carotene project.
+
+## How to install
+
+```bash
+npm i -D flamingo-carotene-vuejs
+```
+
+## How it works
+
+The module copies all the files and directories that are found inside the given source paths to the configured destinations.
+
+## How to configure
+
+This module exposes the following config
---
+++ modules/flamingo-carotene-vuejs/flamingo-carotene-module.js
@@ -0,0 +1,53 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
+
+class FlamingoCaroteneVueJs {
+  constructor (core) {
+    this.config = core.getConfig()
+
+    this.listeners = [
+      {
+        command: 'config',
+        priority: 100,
+        handler: function (core) {
+          const config = core.getConfig()
+
+          config.webpackConfig.resolve.extensions = [
+            ...config.webpackConfig.resolve.extensions,
+            '.vue'
+          ]
+
+          config.webpackConfig.module.rules = [
+            ...config.webpackConfig.module.rules,
+            {
+              test: /\.pug$/,
+              use: [
+                {
+                  loader: 'pug-plain-loader',
+                  options: {
+                    basedir: path.join(config.paths.webpack.src, '../'),
+                  },
+                },
+              ],
+            },
+            {
+              test: /\.vue$/,
+              loader: 'vue-loader',
+            },
+          ]
+
+          config.webpackConfig.plugins = [
+            ...config.webpackConfig.plugins,
+            new VueLoaderPlugin(),
+          ]
+        }
+      },
+    ]
+  }
+
+  getListeners () {
+    return this.listeners
+  }
+}
+
+module.exports = FlamingoCaroteneVueJs
