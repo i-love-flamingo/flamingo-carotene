663765ff4983cc9580abcf8b502f58582d7f7caf
Dylan Provost <dylan.provost@aoe.com>
2020-07-07 11:00:43 +0200

    Refactor vue i18n initialization

--- modules/flamingo-carotene-vuejs/lib/i18n.js
+++ modules/flamingo-carotene-vuejs/lib/i18n.js
@@ -3,8 +3,6 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
-const i18n = new VueI18n({
-  locale: 'default', // we mostly use component based localization (see http://kazupon.github.io/vue-i18n/en/component.html)
+export default new VueI18n({
+  locale: 'default', // we mostly use component based localization (see http://kazupon.github.io/vue-i18n/guide/component.html)
 })
-
-export default i18n
