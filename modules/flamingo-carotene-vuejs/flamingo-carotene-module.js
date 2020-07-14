13fdb0995aa54ded4cabbeaa306f929b38c0b377
Dylan Provost <dylan.provost@aoe.com>
2020-07-14 10:25:22 +0200

    VueJs Add warnings to build

--- modules/flamingo-carotene-vuejs/flamingo-carotene-module.js
+++ modules/flamingo-carotene-vuejs/flamingo-carotene-module.js
@@ -10,8 +10,19 @@ class FlamingoCaroteneVueJs {
         command: 'config',
         priority: -50,
         handler: function (core) {
+          const cliTools = core.getCliTools()
+
+          if (!config.webpackConfig || !config.webpackConfig.resolve || !config.webpackConfig.resolve.extensions) {
+            cliTools.warn('VueJS is configured to use a webpack loader but there is no webpack config available')
+            return
+          }
           // Add vue to the list of extensions WebPack should resolve file names to
           config.webpackConfig.resolve.extensions.push('.vue')
+
+          if (!config.webpackConfig || !config.webpackConfig.module || !config.webpackConfig.module.rules) {
+            cliTools.warn('VueJs is configured to use a webpack loader but there is no webpack config available')
+            return
+          }
 
           // Add new loaders for Vue and Pug/Vue compatibility
           config.webpackConfig.module.rules = [
