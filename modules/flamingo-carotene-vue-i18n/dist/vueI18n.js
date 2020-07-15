a1251547bdd5ae58fa33b99913c600109619eab9
Dylan Provost <dylan.provost@aoe.com>
2020-07-15 14:14:49 +0200

    Temporarily remove flamingo-carotene-vuejs dependency

--- modules/flamingo-carotene-vue-i18n/dist/vueI18n.js
+++ modules/flamingo-carotene-vue-i18n/dist/vueI18n.js
@@ -1,6 +1,6 @@
-import Vue from "flamingo-carotene-vuejs"
+// import Vue from "flamingo-carotene-vuejs"
 import VueI18n from "vue-i18n"
 
-Vue.use(VueI18n)
+// Vue.use(VueI18n)
 
 export default VueI18n
