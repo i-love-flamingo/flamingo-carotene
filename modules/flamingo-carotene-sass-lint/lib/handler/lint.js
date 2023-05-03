37092064d3b080a134bb89c67d0805b6a0ac71c8
dylanprovost <112554171+dylanprovost@users.noreply.github.com>
2023-05-03 15:52:10 +0200

    feat: replace sass-lint (#129)
    * feat: migrate linter to stylelint

--- modules/flamingo-carotene-sass-lint/lib/handler/lint.js
+++ modules/flamingo-carotene-sass-lint/lib/handler/lint.js
@@ -5,31 +5,16 @@ const fs = require('fs')
 const sassLint = (core) => {
   const cliTools = core.getCliTools()
   const config = core.getConfig()
-
-  const timeStarted = new Date().getTime()
-
-  // trying to find sass lint in project folder..
-  let configFile = path.join(config.paths.project, '.sass-lint.yml')
-
-  // if not exists, take standard one
-  if (!fs.existsSync(configFile)) {
-    configFile = path.join(config.paths.sassLint, '.sass-lint.yml')
-  }
-
-
-
   const npmVersion = execSync('npm -v').toString().trim()
 
   core.getJobmanager().addJob('sasslint', 'SASS-Lint', 'webpackCss')
-  // cliTools.info('SassLint - start')
-  const childProcess = core.getSpawner().spawnJobNpx(['sass-lint', '--config', `${configFile}`, '--no-exit', '-v'])
+  const childProcess = core.getSpawner().spawnJobNpx(getCommandParameters(config))
 
   const results = []
   const errors = []
 
   childProcess.stdout.on('data', function (data) {
     // ignore first result line... - cause its the cmd itself
-
     let skipLine = false
 
     // Remove npm version from output
@@ -64,6 +49,7 @@ const sassLint = (core) => {
   childProcess.on('exit', (code) => {
     core.getJobmanager().reportFinishJob('sasslint')
     const output = [].concat(results, errors).join('\n').trim()
+    console.log(output)
 
     if (output.length > 0) {
       if (code !== 0 || errors.length > 0) {
@@ -79,9 +65,60 @@ const sassLint = (core) => {
       }
     }
 
+    cliTools.info('SassLint - params: ' + getCommandParameters(config).join(' '))
+
     core.getJobmanager().finishJob('sasslint')
 
   })
+}
+
+/**
+ * Get the parameters for the cli lint command
+ * @param config
+ * @returns {string[]}
+ */
+const getCommandParameters = function (config) {
+  const parameters = [
+    'stylelint'
+  ]
+
+  // trying to find sass lint in project folder...
+  let configFilePath = path.join(config.paths.project, '.stylelintrc.json')
+
+  // if not exists, take standard one
+  if (!fs.existsSync(configFilePath)) {
+    configFilePath = path.join(config.paths.sassLint, '.stylelintrc.yml')
+  }
+
+  if (configFilePath !== null) {
+    parameters.push('--config', configFilePath)
+  }
+
+  if (config.sassLint.ignoreFilePath) {
+    parameters.push('--ignore-path', config.sassLint.ignoreFilePath)
+  }
+
+  if (config.sassLint.hasOwnProperty('additionalShellParameters') && config.sassLint.additionalShellParameters.length > 0) {
+    for (const param of config.sassLint.additionalShellParameters) {
+      parameters.push(param)
+    }
+  }
+
+  if (config.sassLint.hasOwnProperty('fixErrors') && config.sassLint.fixErrors) {
+    parameters.push('--fix')
+  }
+
+  if (config.sassLint.hasOwnProperty('cache') && config.sassLint.cache) {
+    parameters.push('--cache')
+  }
+
+  if (config.sassLint.hasOwnProperty('maxWarnings') && config.sassLint.maxWarnings) {
+    parameters.push('--mw', config.sassLint.maxWarnings)
+  }
+
+  parameters.push(config.sassLint.fileInclude)
+
+  return parameters
 }
 
 module.exports = sassLint
